0f381cf1b4fbe5f201c0496196ecadb0635a0371
Alexey Nikolaev <leshanikolaev2004@yandex.ru>
2023-05-11 07:29:11 +0300

    Final version

---
+++ src/main/java/org/webApp2/util/PersonValidator.java
@@ -0,0 +1,34 @@
+package org.webApp2.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import org.webApp2.DAO.PersonDAO;
+import org.webApp2.models.Person;
+
+import javax.swing.text.html.HTML;
+
+@Component
+public class PersonValidator implements Validator {
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public PersonValidator(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Person.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        Person person = (Person) target;
+
+        if (personDAO.show(person.getEmail()).isPresent()){
+            errors.rejectValue("email", "", "This email is already taken");
+        }
+    }
+}
